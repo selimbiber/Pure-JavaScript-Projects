fb632b2b141e280db7bd65163f1bed2a73bf2601
selimbiber <117529414+selimbiber@users.noreply.github.com>
2023-07-09 18:44:00 +0300

    finish project of age calculator

---
+++ AgeCalculator/README.md
@@ -0,0 +1,12 @@
+# Age Calculator | Yaş Hesaplayıcı
+
+Bu yaş hesaplama uygulaması ile kullanıcıdan doğum tarihi bilgileri gün, ay ve yıl şeklinde alınıyor. 
+Doğum tarihi bilgileri, güncel tarih bilgisinden çıkartılarak kullanıcının mevcut yaşı hesaplanıyor.
+Kullanıcının mevcut yaşı uygulamanın sonuç kısmında kullanıcıya gösteriliyor. 
+Eğer tarih bilgisi girilmezse uygulamanın sonuç kısmında kullanıcıya uyarı mesajı gösteriliyor.
+
+## Project Description | Proje Açıklaması
+
+Bu uygulamayı [bu adresteki](https://demo.100jsprojects.com/age-calculator) orijinal haline bağlı kalarak yapmaya çalıştım.
+
+### [Preview Link | Canlı Önizleme Bağlantısı]()
---
+++ AgeCalculator/main.js
@@ -0,0 +1,35 @@
+const dateInput = document.getElementById('date-input');
+const submitButton = document.getElementById('submit-button');
+const messageOutput = document.getElementById('message-output');
+
+const date = new Date()
+const currentYear = date.getFullYear(),
+    currentMonth = date.getMonth() +1,
+    currentDay = date.getDate()
+
+// Function
+
+    function calculateAge(birthDate) {
+
+        const birthDateParts = birthDate.split('-');
+        const birthDay = birthDateParts[2];
+        const birthMonth = birthDateParts[1];
+        const birthYear = birthDateParts[0];
+        const yearDifference = currentYear - birthYear;
+
+        let currentAge = yearDifference;
+
+        if (birthMonth > currentMonth || birthMonth == currentMonth && birthDay < currentDay) {
+            return currentAge -1;
+        } else {
+            return currentAge;
+        }
+    }
+
+submitButton.addEventListener('click', (event) => {
+    if (dateInput.value.length === 0) {
+        messageOutput.textContent = 'Please enter your birthday.'
+    } else {
+        messageOutput.textContent = `Your age is ${calculateAge(dateInput.value)} years old.`;
+    }
+})
