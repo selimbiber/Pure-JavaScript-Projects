623ec9f4abb1ef6f787515389e25d8e29602b72c
selimbiber <117529414+selimbiber@users.noreply.github.com>
2023-07-11 22:44:44 +0300

    finish project of random recipes book

---
+++ RandomRecipesBook/README.md
@@ -0,0 +1,9 @@
+# Random Recipes Book | Rastgele Tarifler Kitabı
+
+Bu tarif kitabı uygulaması, sayfa her yenilendiğinde farklı bir yemek tarifini rastgele şekilde kullanıcıya sunuyor. Uygulama için gereken veri [spoonacular.com](https://api.spoonacular.com/recipes/random) adresinden alındı.
+
+## Project Description | Proje Açıklaması
+
+Bu uygulamayı [bu adresteki](https://www.100jsprojects.com/project/recipe-book-app) orijinal halini örnek alarak alarak yaptım. Gelecekte uygulamaya daha fazla sayıda özellik ekleyerek projeyi geliştirmek niyetindeyim.
+
+### [Preview Link | Canlı Önizleme Bağlantısı]()
---
+++ RandomRecipesBook/main.js
@@ -0,0 +1,46 @@
+const container = document.querySelector('.container')
+const card = document.querySelector('.card')
+
+fetch("https://api.spoonacular.com/recipes/random?apiKey=0adc62c089b94d65bbbd11419bee9e4e")
+  .then(response => response.json())
+  .then(data => {
+    const recipe = data.recipes[0];
+
+      const cardFigure = document.createElement('figure');
+      const cardFigureImage = document.createElement('img');
+      cardFigureImage.setAttribute('src', recipe.image);
+      cardFigureImage.setAttribute('alt', recipe.title);
+      cardFigure.appendChild(cardFigureImage);
+      card.appendChild(cardFigure);
+
+      const cardFigcaptionName = document.createElement('figcaption');
+      cardFigcaptionName.setAttribute('class', 'recipe-name');
+      cardFigcaptionName.textContent = recipe.title;
+      card.appendChild(cardFigcaptionName);
+      
+      const recipeIngListTitle = document.createElement('h4');
+      recipeIngListTitle.setAttribute('class', 'list-title')
+      recipeIngListTitle.textContent = 'Ingredients:';
+    card.appendChild(recipeIngListTitle);
+
+      const recipeList = document.createElement('ul');
+      recipeList.setAttribute('class', 'list');
+      card.appendChild(recipeList);
+
+      recipe.extendedIngredients.forEach((ingredient) => {
+        const ingredientLi = document.createElement('li');
+        ingredientLi.textContent = ingredient.original;
+        recipeList.appendChild(ingredientLi);
+      });
+
+      const actionButton = document.createElement('button');
+      actionButton.setAttribute('class', 'action-btn');
+      actionButton.textContent = 'VIEW RECIPE';
+      actionButton.addEventListener('click', () => {
+        window.open(recipe.sourceUrl, '_blank');
+      });
+      card.appendChild(actionButton);
+      
+      card.style.display = 'flex';
+  })
+  .catch(error => alert(error));
