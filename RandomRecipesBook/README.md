87f959e131887a2c9810160e25653f2aba2b06f8
selimbiber <117529414+selimbiber@users.noreply.github.com>
2023-07-12 17:18:33 +0300

    Update project of random recipes book to 2.0

--- RandomRecipesBook/README.md
+++ RandomRecipesBook/README.md
@@ -6,4 +6,10 @@ Bu tarif kitabı uygulaması, sayfa her yenilendiğinde farklı bir yemek tarifi
 
 Bu uygulamayı [bu adresteki](https://www.100jsprojects.com/project/recipe-book-app) orijinal halini örnek alarak alarak yaptım. Gelecekte uygulamaya daha fazla sayıda özellik ekleyerek projeyi geliştirmek niyetindeyim.
 
+### Updates | Güncellemeler
+
+Uygulamanın ilk sürümünde sadece bir adet tarif ekrana getiriyordu fakat şimdiki güncelleme ile 1 ile 9 adet arasında değişen sayıda farklı tarifi, uygulama sayfası her yenilendiğinde ekrana getiriyor. 
+
+Bir sonraki güncellemede ise, uygulamanın veriyi çekmesinde bir sorun oluşması halinde kullanıcıya gösterilecek hata mesajı geliştirilecek ve VIEW RECIPE butonu VIEW PREPARATION butonuna dönüştürülüp kullanıcı butona tıkladığında ekranda açılacak modal kutuda ilgili tarifin hazırlanış aşamaları yer alacak ve böylelikle kullanıcı farklı bir sayfaya yönlendirilmeyecek.
+
 ### [Preview Link | Canlı Önizleme Bağlantısı]()
--- RandomRecipesBook/main.js
+++ RandomRecipesBook/main.js
@@ -1,46 +1,50 @@
-const container = document.querySelector('.container')
-const card = document.querySelector('.card')
+const cardsContainer = document.querySelector('.cards-container')
 
-fetch("https://api.spoonacular.com/recipes/random?apiKey=0adc62c089b94d65bbbd11419bee9e4e")
-  .then(response => response.json())
-  .then(data => {
-    const recipe = data.recipes[0];
+for ( let i = 0; i < Math.floor(Math.random() * 10); i++) {
+  fetch("https://api.spoonacular.com/recipes/random?apiKey=0adc62c089b94d65bbbd11419bee9e4e")
+    .then(response => response.json())
+    .then(data => {
+      const recipe = data.recipes[0];
+  const card = document.createElement('div');
+  card.setAttribute('class', 'card');
+  cardsContainer.appendChild(card);
 
-      const cardFigure = document.createElement('figure');
-      const cardFigureImage = document.createElement('img');
-      cardFigureImage.setAttribute('src', recipe.image);
-      cardFigureImage.setAttribute('alt', recipe.title);
-      cardFigure.appendChild(cardFigureImage);
-      card.appendChild(cardFigure);
+  const cardFigure = document.createElement('figure');
+  const cardFigureImage = document.createElement('img');
+  cardFigureImage.setAttribute('src', recipe.image);
+  cardFigureImage.setAttribute('alt', recipe.title);
+  cardFigure.appendChild(cardFigureImage);
+  card.appendChild(cardFigure);
 
-      const cardFigcaptionName = document.createElement('figcaption');
-      cardFigcaptionName.setAttribute('class', 'recipe-name');
-      cardFigcaptionName.textContent = recipe.title;
-      card.appendChild(cardFigcaptionName);
-      
-      const recipeIngListTitle = document.createElement('h4');
-      recipeIngListTitle.setAttribute('class', 'list-title')
-      recipeIngListTitle.textContent = 'Ingredients:';
-    card.appendChild(recipeIngListTitle);
+  const cardFigcaptionName = document.createElement('figcaption');
+  cardFigcaptionName.setAttribute('class', 'recipe-name');
+  cardFigcaptionName.textContent = recipe.title;
+  card.appendChild(cardFigcaptionName);
+  
+  const recipeIngListTitle = document.createElement('h4');
+  recipeIngListTitle.setAttribute('class', 'list-title')
+  recipeIngListTitle.textContent = 'Ingredients:';
+  card.appendChild(recipeIngListTitle);
 
-      const recipeList = document.createElement('ul');
-      recipeList.setAttribute('class', 'list');
-      card.appendChild(recipeList);
+  const recipeList = document.createElement('ul');
+  recipeList.setAttribute('class', 'list');
+  card.appendChild(recipeList);
 
-      recipe.extendedIngredients.forEach((ingredient) => {
-        const ingredientLi = document.createElement('li');
-        ingredientLi.textContent = ingredient.original;
-        recipeList.appendChild(ingredientLi);
-      });
+  recipe.extendedIngredients.forEach((ingredient) => {
+    const ingredientLi = document.createElement('li');
+    ingredientLi.textContent = ingredient.original;
+    recipeList.appendChild(ingredientLi);
+  });
 
-      const actionButton = document.createElement('button');
-      actionButton.setAttribute('class', 'action-btn');
-      actionButton.textContent = 'VIEW RECIPE';
-      actionButton.addEventListener('click', () => {
-        window.open(recipe.sourceUrl, '_blank');
-      });
-      card.appendChild(actionButton);
-      
-      card.style.display = 'flex';
+  const actionButton = document.createElement('button');
+  actionButton.setAttribute('class', 'action-btn');
+  actionButton.textContent = 'VIEW RECIPE';
+  actionButton.addEventListener('click', () => {
+    window.open(recipe.sourceUrl, '_blank');
+  });
+  card.appendChild(actionButton);
+  
+  card.style.display = 'flex';
   })
   .catch(error => alert(error));
+}
