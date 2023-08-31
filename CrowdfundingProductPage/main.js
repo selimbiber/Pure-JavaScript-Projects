e432a279e64fc6fb80cf984a4d64f76c95850b8c
selimbiber <117529414+selimbiber@users.noreply.github.com>
2023-09-01 01:49:04 +0300

    Finish project of Crowdfunding Product Page.

--- CrowdfundingProductPage/main.js
+++ CrowdfundingProductPage/main.js
@@ -18,22 +18,13 @@ const ENTER_PLEDGE_NO_REWARD = document.getElementById('enter-pledge_no-reward')
 const ENTER_PLEDGE_BAMBOO_STAND = document.getElementById('enter-pledge_bamboo-stand')
 const ENTER_PLEDGE_BLACK_EDITION = document.getElementById('enter-pledge_black-edition')
 
-const LEFT_BEHIND_ABOUT_SECTION = document.querySelectorAll('div .left-behind')
-LEFT_BEHIND_ABOUT_SECTION.forEach( (leftBehind) => {
-  if (leftBehind.textContent == '0') {
-    leftBehind.parentElement.parentElement.parentElement.classList.add('finished_reward_list-items')
-  } else {
-    leftBehind.parentElement.parentElement.parentElement.classList.remove('finished_reward_list-items')
-  }
-} );
-
 /* MODAL MENU SUBSECTION */
 const MODAL_MENU = document.getElementById('modal-menu-subsection')
 const MODAL_MENU_BTN = document.getElementById('modal-menu_btn')
 const REWARD_LIST_ITEMS = document.querySelectorAll('.select-reward_list-items')
 const PLEDGE_AMOUNT_INPUTS = document.querySelectorAll('.pledge-amount_input')
 const CONTINUE_BUTTONS = document.querySelectorAll('.continue-btn')
-const GOT_BTN = document.getElementById('got-it-btn')
+const GOT_BTN = document.getElementById('got-it_btn')
 const SUCCCESS_MODAL_SUBSECTION = document.getElementById('success-modal-subsection')
 const RADIO_BUTTONS = document.querySelectorAll('.input-radio')
 
@@ -57,6 +48,9 @@ function updateCurrentBackedRate (input) {
 
   CURRENT_BACKED_RATE.textContent = currentBackedRate.toLocaleString();
   TOTAL_BACKERS_COUNT.textContent = totalBackersCount.toLocaleString();
+
+  const PERCENTAGE = (currentBackedRate / 100000) * 100;
+  PROGRESS_BAR_STATE.style.width = PERCENTAGE + '%';
 }
 
 let numberDaysLeft = 56;
@@ -113,14 +107,19 @@ function removeBorderColor() {
   });
 }
 
-// GOT_BTN.addEventListener('click', () => {
-//     SUCCCESS_MODAL_SUBSECTION.classList.remove('open-modal');
-//     MENU_BACKGROUND.classList.remove('menu-bg_visible');
-// });
-
 const NO_REWARD_PLEDGE_FORM = document.getElementById('no-reward_pledge-form')
 NO_REWARD_PLEDGE_FORM.addEventListener('submit', (submit) => {
   submit.preventDefault();
+  if (ENTER_PLEDGE_NO_REWARD.value < 1) {
+    const WM = document.querySelector('#warning-message_no-reward')
+    WM.style.display = 'block';
+  } else {
+    const WM = document.querySelector('#warning-message_no-reward')
+    WM.style.display = 'none';
+    updateCurrentBackedRate(ENTER_PLEDGE_NO_REWARD.value);
+    toggleModalMenu();
+    showSuccessMessage();
+  }
 });
 
 const BAMBOO_STAND_PLEDGE_FORM = document.getElementById('bamboo-stand_pledge-form')
@@ -136,9 +135,8 @@ BAMBOO_STAND_PLEDGE_FORM.addEventListener('submit', (submit) => {
     BAMBOO_STAND_LEFT_BEHIND.forEach( (leftBehind) => {
       leftBehind.textContent = Number(leftBehind.textContent) -1;
       if (leftBehind.textContent == '0') {
-        leftBehind.parentElement.parentElement.classList.add('finished_reward_list-items')
-      } else {
-        leftBehind.parentElement.parentElement.classList.remove('finished_reward_list-items')
+        leftBehind.parentElement.parentElement.classList.add('finished_reward_list-items');
+        document.querySelector('span.bamboo-stand_left-behind').parentElement.parentElement.parentElement.classList.add('finished_reward_list-items');
         ENTER_PLEDGE_BAMBOO_STAND.disabled = true;
         const BTN = document.getElementById('bamboo-stand_continue-btn');
         BTN.disabled = true;
@@ -148,8 +146,9 @@ BAMBOO_STAND_PLEDGE_FORM.addEventListener('submit', (submit) => {
       }
     } );
     toggleModalMenu();
+    showSuccessMessage();
   }
-} )
+} );
 
 const BLACK_EDITION_PLEDGE_FORM = document.getElementById('black-edition_pledge-form')
 BLACK_EDITION_PLEDGE_FORM.addEventListener('submit', (submit) => {
@@ -164,7 +163,8 @@ BLACK_EDITION_PLEDGE_FORM.addEventListener('submit', (submit) => {
     BLACK_EDITION_LEFT_BEHIND.forEach( (leftBehind) => {
       leftBehind.textContent = Number(leftBehind.textContent) -1;
       if (leftBehind.textContent == '0') {
-        leftBehind.parentElement.parentElement.classList.add('finished_reward_list-items')
+        leftBehind.parentElement.parentElement.classList.add('finished_reward_list-items');
+        document.querySelector('span.black-edition_left-behind').parentElement.parentElement.parentElement.classList.add('finished_reward_list-items');
         ENTER_PLEDGE_BLACK_EDITION.disabled = true;
         const BTN = document.getElementById('black-edition_continue-btn');
         BTN.disabled = true;
@@ -174,6 +174,7 @@ BLACK_EDITION_PLEDGE_FORM.addEventListener('submit', (submit) => {
       }
     } );
     toggleModalMenu();
+    showSuccessMessage();
   }
 });
 
@@ -219,39 +220,21 @@ PLEDGE_AMOUNT_INPUTS.forEach( (input) => {
     }, false)
 } );
 
-/*
-const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-const screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+function showSuccessMessage () {
+  MENU_BACKGROUND.classList.add('menu-bg_visible');
+  SUCCCESS_MODAL_SUBSECTION.style.display = 'flex';
 
-Bir öğeyi otomatik olarak kullanıcının ekranının tam ortasında konumlandırmak için aşağıdaki adımları takip edebilirsiniz:
+  centerComponentOnScreen();
 
-1. Kullanıcının ekranının genişlik ve yükseklik değerlerini alın.
-2. Öğenin genişlik ve yükseklik değerlerini alın.
-3. Ekranın genişlik ve öğenin genişlik değerleri arasındaki farkı hesaplayın ve 2'ye bölün.
-4. Ekranın yükseklik ve öğenin yükseklik değerleri arasındaki farkı hesaplayın ve 2'ye bölün.
-5. Bu hesaplanan değerleri öğenin sol ve üst kenarlarına ekleyerek öğenin tam ortasını bulun.
-6. Bu konumu kullanarak öğeyi görüntüleyin.
+  GOT_BTN.addEventListener('click', () => {
+    SUCCCESS_MODAL_SUBSECTION.style.display = 'none';
+    MENU_BACKGROUND.classList.remove('menu-bg_visible');
+  });
+}
 
-Örneğin, JavaScript dilinde aşağıdaki gibi bir kod kullanabilirsiniz:
-
-```javascript
-var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-var screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-
-var element = document.getElementById("elementId");
-var elementWidth = element.offsetWidth;
-var elementHeight = element.offsetHeight;
-
-var leftPosition = (screenWidth - elementWidth) / 2;
-var topPosition = (screenHeight - elementHeight) / 2;
-
-element.style.left = leftPosition + "px";
-element.style.top = topPosition + "px";
-```
-
-Bu kod, "elementId" olarak tanımlanan öğeyi kullanıcının ekranının tam ortasında konumlandıracaktır. Kodu web sayfanızda kullanarak öğeyi görüntüleyebilirsiniz.
-
-*/
-
-const PERCENTAGE = (currentBackedRate / 100000) * 100;
-PROGRESS_BAR_STATE.style.width = PERCENTAGE + '%';
+function centerComponentOnScreen () {
+  SUCCCESS_MODAL_SUBSECTION.style.position = 'fixed';
+  SUCCCESS_MODAL_SUBSECTION.style.top = '50%';
+  SUCCCESS_MODAL_SUBSECTION.style.left = '50%';
+  SUCCCESS_MODAL_SUBSECTION.style.transform = 'translate(-50%, -50%)';
+}
