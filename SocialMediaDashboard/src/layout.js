c7d5584c7634512be996fdc3e254e39fd307098c
selimbiber <117529414+selimbiber@users.noreply.github.com>
2023-09-16 23:53:34 +0300

    fix icon problem

--- SocialMediaDashboard/src/layout.js
+++ SocialMediaDashboard/src/layout.js
@@ -74,7 +74,7 @@ function createFooterSection() {
         FOOTER_SECTION.appendChild(GITHUB_REPO_LINK);
 
             const GITHUB_LOGO_ICON = document.createElement('i');
-                GITHUB_LOGO_ICON.classList.add('fa-brands', 'fa-github');
+                GITHUB_LOGO_ICON.classList.add('fab', 'fa-github');
             GITHUB_REPO_LINK.appendChild(GITHUB_LOGO_ICON);
 
         const GITHUB_PROFILE_LINK = document.createElement('a');
