b297315cef62f0a5c5eb9b38959b8f6c653dab8f
selimbiber <117529414+selimbiber@users.noreply.github.com>
2023-09-29 22:34:24 +0300

    Update project of Sign-up Form

--- Sign-upForm/main.js
+++ Sign-upForm/main.js
@@ -1,60 +1,66 @@
+const SIGN_UP_FORM = document.getElementById('sign-up-form');
+
 /* INPUTS */
 const FIRST_NAME = document.getElementById('first-name')
 const LAST_NAME = document.getElementById('last-name')
-const E_MAIL = document.getElementById('e-mail')
+const EMAIL = document.getElementById('e-mail')
 const PASSWORD = document.getElementById('password')
 
 /* WARNINGS */
 const FIRST_NAME_WM = document.getElementById('first-name_warning-message')
 const LAST_NAME_WM = document.getElementById('last-name_warning-message')
-const E_MAIL_WM = document.getElementById('e-mail_warning-message')
+const EMAIL_WM = document.getElementById('e-mail_warning-message')
 const PASSWORD_WM = document.getElementById('password_warning-message')
 
-/* EVENTS */
-FIRST_NAME.addEventListener('input', (e) => {
-  showWarningMessage('first-name', e.target.value.length)
-});
-LAST_NAME.addEventListener('input', (e) => {
-  showWarningMessage('last-name', e.target.value.length)
-});
-E_MAIL.addEventListener('input', (e) => {
-  showWarningMessage('e-mail', e.target.value.length)
-});
-PASSWORD.addEventListener('input', (e) => {
-  showWarningMessage('password', e.target.value.length)
+/* VALIDATION */
+SIGN_UP_FORM.addEventListener('submit', (f) => {
+  f.preventDefault();
+
+  showWarningMessage('first-name', FIRST_NAME.value.length);
+  showWarningMessage('last-name', LAST_NAME.value.length);
+  showWarningMessage('e-mail', EMAIL.value.length);
+  showWarningMessage('password', PASSWORD.value.length);
 });
 
 /* FUNCTIONS */
 function showWarningMessage (input, length) {
   if (input === 'first-name' && length == 0) {
       FIRST_NAME.placeholder = '';
+      FIRST_NAME.className = 'invalid-input';
       FIRST_NAME_WM.style.display = 'block';
-      FIRST_NAME_WM.innerText = 'First Name cannot be empty';
+      FIRST_NAME_WM.textContent = 'First Name cannot be empty';
   } else if (input === 'first-name' && length > 0) {
       FIRST_NAME_WM.style.display = 'none';
+      FIRST_NAME.removeAttribute('class');
   }
   if (input === 'last-name' && length == 0) {
       LAST_NAME.placeholder = '';
+      LAST_NAME.className = 'invalid-input';
       LAST_NAME_WM.style.display = 'block';
-      LAST_NAME_WM.innerText = 'Last Name cannot be empty';
+      LAST_NAME_WM.textContent = 'Last Name cannot be empty';
   } else if (input === 'last-name' && length > 0) {
       LAST_NAME_WM.style.display = 'none';
+      LAST_NAME.removeAttribute('class');
   }
-  if ( isEmailValid(E_MAIL.value) == false ) {
-      E_MAIL.placeholder = 'email@example/com'
-      E_MAIL.style.setProperty('--email-placeholder-color', '#e49090');
-      E_MAIL.style.setProperty('--email-placeholder-fontsize', '1.5rem');
-      E_MAIL_WM.style.display = 'block';
-      E_MAIL_WM.innerText = 'Looks like this is not an email';
-  } else if ( isEmailValid(E_MAIL.value) == true ) {
-      E_MAIL_WM.style.display = 'none';
+  if ( isEmailValid(EMAIL.value) == false ) {
+      EMAIL.placeholder = 'email@example/com';
+      EMAIL.className = 'invalid-input';
+      EMAIL.style.setProperty('--email-placeholder-color', '#e49090');
+      EMAIL.style.setProperty('--email-placeholder-fontsize', '1.5rem');
+      EMAIL_WM.style.display = 'block';
+      EMAIL_WM.textContent = 'Looks like this is not an email';
+  } else if ( isEmailValid(EMAIL.value) == true ) {
+      EMAIL_WM.style.display = 'none';
+      EMAIL.removeAttribute('class');
   }
   if (input === 'password' && length == 0) {
       PASSWORD.placeholder = '';
+      PASSWORD.className = 'invalid-input';
       PASSWORD_WM.style.display = 'block';
-      PASSWORD_WM.innerText = 'Password cannot be empty';
+      PASSWORD_WM.textContent = 'Password cannot be empty';
   } else if (input === 'password' && length > 0) {
       PASSWORD_WM.style.display = 'none';
+      PASSWORD.removeAttribute('class');
   }
 }
 
