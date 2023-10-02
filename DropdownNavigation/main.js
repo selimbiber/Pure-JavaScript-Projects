433ae7f695697d3a76eba7774e2ad083bfa96526
selimbiber <117529414+selimbiber@users.noreply.github.com>
2023-10-02 21:31:56 +0300

    last update for project of dropdown navigation

--- DropdownNavigation/main.js
+++ DropdownNavigation/main.js
@@ -1,14 +1,9 @@
-const BODY_CONTAINER = document.getElementById('container')
 const HEADER_SECTION = document.getElementById('header-section')
 const HAMBURGER_MENU = document.getElementById('header-section_nav-menu')
-const HAMBURGER_BTN = document.getElementById('hamburger-menu_btn')
+const HAMBURGER_MENU_BTN = document.getElementById('hamburger-menu_btn')
 const ICON_OPEN_MENU = document.getElementById('icon-open-menu')
 const ICON_CLOSE_MENU = document.getElementById('icon-close-menu')
-const HERO_IMAGE = document.getElementById('hero-section_mobile-image')
-const HERO_TEXT = document.getElementById('content-subsection_article-text')
-const HERO_BTN = document.getElementById('content-subsection_article-btn')
 const DROPDOWN_ITEMS = document.querySelectorAll('.dropdown-items')
-const CLIENT_ICONS = document.querySelectorAll('.client-icons')
 
 function createPageBackground() {
   if ( document.querySelector('.page-background') ) document.querySelector('.page-background').remove();
@@ -17,23 +12,31 @@ function createPageBackground() {
   document.body.insertBefore(PAGE_BACKGROUND, HEADER_SECTION);
 }
 
-if (window.innerWidth < 1440) {
-  function toggleMenu() {
-    if ( HAMBURGER_MENU.classList.contains("show-menu") ) {
-      HAMBURGER_MENU.classList.remove("show-menu");
-      document.querySelector('.page-background').remove();
-    } else {
-      HAMBURGER_MENU.classList.add("show-menu");
-      ICON_CLOSE_MENU.style.display = "block";
-      ICON_OPEN_MENU.style.display = "none";
-      createPageBackground();
-    }
-  }
-} else {
-  function toggleMenu() {
+const MAX_WIDTH = window.matchMedia('(max-width: 1440px)');
+
+function toggleMenu(media) {
+  if (media.matches) {
+    HAMBURGER_MENU_BTN.addEventListener('click', () => {
+      if ( HAMBURGER_MENU.classList.contains("show-menu") ) {
+        HAMBURGER_MENU.classList.remove("show-menu");
+        ICON_CLOSE_MENU.style.display = "none";
+        ICON_OPEN_MENU.style.display = "block";
+        document.querySelector('.page-background').remove();
+      } else {
+        HAMBURGER_MENU.classList.add("show-menu");
+        ICON_CLOSE_MENU.style.display = "block";
+        ICON_OPEN_MENU.style.display = "none";
+        createPageBackground();
+      }
+    });
+  } else {
     return false;
   }
 }
+
+toggleMenu(MAX_WIDTH);
+
+MAX_WIDTH.addEventListener(toggleMenu);
 
 DROPDOWN_ITEMS.forEach((targetDetail) => {
   targetDetail.addEventListener("click", () => {
