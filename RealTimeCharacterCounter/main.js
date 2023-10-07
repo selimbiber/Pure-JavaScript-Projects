cc61f767067a14c8e190120a9d800004f41b33a1
selimbiber <117529414+selimbiber@users.noreply.github.com>
2023-10-07 12:39:01 +0300

    finish last step for the project of real-time chars counter

--- RealTimeCharacterCounter/main.js
+++ RealTimeCharacterCounter/main.js
@@ -3,13 +3,8 @@ const TEXT_AREA = document.getElementById('character-counter-textarea');
 const TOTAL_CHARS_COUNT_DOM = document.querySelector('.total-characters-count');
 const REMAINING_CHARS_COUNT_DOM = document.querySelector('.remaining-characters-count');
 
-let totalCharsCountNum = Number(TOTAL_CHARS_COUNT_DOM.textContent);
-let remainingCharsCountNum = Number(REMAINING_CHARS_COUNT_DOM.textContent);
-
 TEXT_AREA.addEventListener('input', (e) => {
-    totalCharsCountNum += Number(e.target.value.length);
-    TOTAL_CHARS_COUNT_DOM.textContent = totalCharsCountNum;
+    TOTAL_CHARS_COUNT_DOM.textContent = Number(e.target.value.length);
     
-    remainingCharsCountNum -= Number(e.target.value.length);
-    REMAINING_CHARS_COUNT_DOM.textContent = remainingCharsCountNum;
+    REMAINING_CHARS_COUNT_DOM.textContent = 50 -  Number(e.target.value.length);
 });
