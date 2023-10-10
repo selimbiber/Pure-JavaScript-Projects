549a1e9167da090b5a4940debe5200da1aefadaa
selimbiber <117529414+selimbiber@users.noreply.github.com>
2023-10-10 23:00:53 +0300

    add dynamic title attributes for button elements.

--- Q-A-Section/main.js
+++ Q-A-Section/main.js
@@ -5,9 +5,11 @@ document.querySelectorAll('button').forEach(button => {
         if ( ARTICLE_ELEMENT.classList.contains('open') ) {
             ARTICLE_ELEMENT.removeAttribute('class');
             ARTICLE_ELEMENT.querySelector('i').classList.replace('fa-minus-square', 'fa-plus-square');
+            ARTICLE_ELEMENT.querySelector('button').title = 'Click this button for open the text area';
         } else {
             ARTICLE_ELEMENT.className = 'open';
             ARTICLE_ELEMENT.querySelector('i').classList.replace('fa-plus-square', 'fa-minus-square');
+            ARTICLE_ELEMENT.querySelector('button').title = 'Click this button for close the text area';
         }
-    })
-})
+    });
+});
